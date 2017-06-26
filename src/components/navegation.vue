c0311dd14be4c3ebbfac4434fed05bcccc118e12
Unknown <rafaelplmartins@hotmail.com>
2017-06-26 17:59:36 -0300

    Dropdown add, and init tests

--- src/components/navegation.vue
+++ src/components/navegation.vue
@@ -3,16 +3,25 @@
   <div class="row">
 
 
-    <div class="col-md-3 col-sm-3 logo-name">
+    <div class="col-md-3 logo-name">
       <img src="https://cdn2.iconfinder.com/data/icons/file-types-7/32/md-markdown-filetype-document-file-filetype-document-file-512.png" height="40px" width="40px">
       MarkiDown
     </div>
 
-    <div class="col-md-4 col-md-offset-1">
+    <div class="col-md-5">
 
       <ul class="nav nav-pills nav-justified">
         <li role="presentation" class="active"><a href="#">Home</a></li>
         <li role="presentation"><a href="#">Import MD</a></li>
+
+        <li role="presentation" :class="['dropdown', dropdownVisible ? 'open' : '']" @click="toggle">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+            Start from Template <span class="caret"></span>
+          </a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Docs</a></li>
+          </ul>
+        </li>
         <li role="presentation"><a href="#">About</a></li>
       </ul>
 
@@ -22,11 +31,18 @@
 </template>
 
 <script>
+
 export default {
   name: 'navigation',
   data() {
     return {
+      dropdownVisible: null,
     };
+  },
+  methods: {
+    toggle() {
+      this.dropdownVisible = !this.dropdownVisible;
+    },
   },
 };
 </script>
