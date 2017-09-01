4436b67f0750ee920a1db7ea48ee13389786db44
Unknown <rafaelplmartins@hotmail.com>
2017-08-31 21:37:03 -0300

    feat: env variable to pass key

---
+++ src/services/apiKeys.js
@@ -0,0 +1,8 @@
+const config = {
+  apiKey: process.env.DB_SECRET,
+  authDomain: 'merkidown.firebaseapp.com',
+  databaseURL: 'https://merkidown.firebaseio.com/',
+  projectId: 'merkidown',
+};
+
+export { config };
